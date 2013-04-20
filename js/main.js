31645336c76f0a88bcabade03e0aa1abd6941363
Ruan YiFeng <yifeng.ruan@gmail.com>
2013-04-20 11:32:59 +0800

    edit counter function

--- js/main.js
+++ js/main.js
@@ -1,21 +1,25 @@
 jQuery(document).on('click',"header a",function(){
-			
-			$('header').hide(600).show(600);
-			});
+	$('header')
+	.animate({width:0,height:0,opacity:0},700)
+	.animate({width:"100%",height:"100%",opacity:1}, 700);
+});
 
 jQuery(document).on('click',"#toc a",function(){
-			
-			$('header').hide(600).show(600);
-			});
+	$('header')
+	.animate({width:0,height:0,opacity:0},700)
+	.animate({width:"100%",height:"100%",opacity:1}, 700);
+});
 
 jQuery(document).ready(function(){
 
 			$('#toc').toc({
 				'selectors': 'h2,h3,h4', //elements to use as headings
-    'container': 'article'	
-	});
+				'container': 'article'
+			});
+
+			$('#toc ul li.toc-h2').clone().appendTo("nav section li.nav-3 ul");
 			
-		$('#toc').before('<h2>目录</h2>');	
+			$('#toc').before('<h2>目录</h2>');	
 
 /*
         $('#toc li.toc-h2 a').each(function (i){
